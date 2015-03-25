7dba79b9d5cddbf61406505ecff118123a852809
Yi Han <hanystudy@gmail.com>
2015-03-25 08:21:59 +0800

    [hy] add ruby, memcached, mysql to repo

---
+++ playbooks/files/memcached.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /home/vagrant/memcached-1.4.22
+
+./configure && make && make install
---
+++ playbooks/files/rbenv.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
