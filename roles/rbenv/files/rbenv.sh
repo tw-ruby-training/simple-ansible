cb51635a41a29e393b24275687a301e1743b32d2
Yi Han <hanystudy@gmail.com>
2015-03-26 14:27:28 +0800

    [hy] add remote node as candidate

--- roles/rbenv/files/rbenv.sh
+++ roles/rbenv/files/rbenv.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-EXPORT_PATH = 'export PATH="$HOME/.rbenv/bin:$PATH"'
-EVAL_INIT = 'eval "$(rbenv init -)"'
 
-function write_bash_profile()
-	if grep -Fxq $0
+EXPORT_PATH='export PATH="$HOME/.rbenv/bin:$PATH"'
+EVAL_INIT='eval "$(rbenv init -)"'
+
+function write_bash_profile() {
+	if grep -Fxq $1 ~/.bash_profile
 	then
+		echo 'PASS'
 	else	
 		echo $0 >> ~/.bash_profile
 	fi
-end
+}
 
-write_bash_profile($EXPORT_PATH)
-write_bash_profile($EVAL_INIT)
+write_bash_profile $EXPORT_PATH
+write_bash_profile $EVAL_INIT
 
-exit(0)
+exit 0
